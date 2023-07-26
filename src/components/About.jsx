9fb5b9952008e2feb63f0c0454ad28aa3cef359d
Mark Harji Mating <mark.mating@flyfreely.io>
2023-07-26 12:50:43 +0800

    center about

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -49,7 +49,7 @@ const About = () => {
         the backend
       </motion.p>
 
-      <div className="mt-20 flex flex-wrap gap-10">
+      <div className="mt-20 flex flex-wrap gap-10 justify-center">
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
         ))}
