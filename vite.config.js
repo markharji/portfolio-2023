c372b0fd015bf2265a989934346989b84158da5a
Mark Harji Mating <mark.mating@flyfreely.io>
2023-07-26 12:13:46 +0800

    deploy

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  base: "https://github.com/markharji/portfolio-2023",
+export default defineConfig(({ command }) => {
+  const config = {
+    plugins: [react()],
+    base: "/",
+  };
+
+  if (command !== "serve") {
+    config.base = "/portfolio-2023/";
+  }
+
+  return config;
 });
